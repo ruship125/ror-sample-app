11b39390e14e28d9cb943a6533fadc4d76a4f909
ruship <rushipawar1234@gmail.com>
2016-10-27 00:20:31 +0530

    Finish user edit,update,index and destory options

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,10 @@
 module UsersHelper
 
-	def gravatar_for(user, size: 80)
-		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-		gravatar_url = "http://i.imgur.com/4SrBjCm.jpg#{gravatar_id}?s=#{size}"
-		image_tag(gravatar_url, alt: user.name, class: "gravatar")
-	end
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+	gravatar_url = "http://i.imgur.com/4SrBjCm.jpg#{gravatar_id}?s=#{size}"
+	image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
