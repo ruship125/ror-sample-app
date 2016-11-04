78571b4721139d9a8864b737ea78745c3c15e0e0
ruship <rushipawar1234@gmail.com>
2016-11-04 19:16:36 +0530

    Fixed account_activation_contoller which gave flash error

--- app/controllers/account_activation_controller.rb
+++ app/controllers/account_activation_controller.rb
@@ -5,10 +5,10 @@ class AccountActivationController < ApplicationController
 		if user && !user.activated? && user.authenticatedd?(:activation, params[:id])
 			user.acitvate 
 			log_in user
-			flash [:success] = "Account activated!"
+			flash[:success] = "Account activated!"
 			redirect_to user
 		else
-			flash [:danger] = "Invalid activation link"
+			flash[:danger] = "Invalid activation link"
 			redirect_to root_url
 		end
 	end
